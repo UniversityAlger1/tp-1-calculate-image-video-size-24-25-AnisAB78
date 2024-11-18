28547ea300869d0d23c3119416f5090af4d0ac6c
AnisAB78 <anisetude00@gmail.com>
2024-11-18 15:49:29 +0100

    coloredBitmap.c

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -1,31 +1,37 @@
 #include "config/coloredBitmap.h"
 #include <string.h>
+#include <stdio.h>
 
 // Parameters:
 //   w: width of the image
 //   h: height of the image
 //   unit: Unit of the output value. It could be 'bt' bits, 'ko' kilobits, 'mo' megabits, 'go' gigabits
-// Return value:
+// Return value
 //   colored image size Bitmap (based on the unit passed parameter)
 float coloredBitmap(int w, int h, char* unit) {
-    // Each pixel of a colored Bitmap uses 24 bits (8 bits per color channel: R, G, B)
-    const int bits_per_pixel = 24; 
-    long long total_bits = (long long)w * h * bits_per_pixel;
+    // Determine the number of bits per pixel (assuming 24 bits for RGB)
+    int bitsPerPixel = 24;
 
+    // Calculate the total number of bits
+    int totalBits = w * h * bitsPerPixel;
+
+    // Variable to hold the size in the specified unit
+    float size;
+
+    // Determine the size based on the unit
     if (strcmp(unit, "bt") == 0) {
-        // Return size in bits
-        return (float)total_bits;
+        size = totalBits; // Size in bits
     } else if (strcmp(unit, "ko") == 0) {
-        // Convert bits to kilobits: 1 kilobit = 1024 bits
-        return (float)total_bits / 1024;
+        size = totalBits / (1024.0 * 8); // Size in kilobits
     } else if (strcmp(unit, "mo") == 0) {
-        // Convert bits to megabits: 1 megabit = 1024 * 1024 bits
-        return (float)total_bits / (1024 * 1024);
+        size = totalBits / (1024.0 * 1024.0 * 8); // Size in megabits
     } else if (strcmp(unit, "go") == 0) {
-        // Convert bits to gigabits: 1 gigabit = 1024 * 1024 * 1024 bits
-        return (float)total_bits / (1024 * 1024 * 1024);
+        size = totalBits / (1024.0 * 1024.0 * 1024.0 * 8); // Size in gigabits
     } else {
-        // Invalid unit
+        // If the unit is not recognized, return -1 as an error code
+        printf("Invalid unit specified.\n");
         return -1;
     }
+
+    return size; // Return the calculated size
 }
